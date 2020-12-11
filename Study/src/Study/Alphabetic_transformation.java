a702c4701fbe14115dfca85547b9a101da55dcb9
Hyeonmin <kahelum@gmail.com>
2020-12-12 01:26:33 +0900

    commit

---
+++ Study/src/Study/Alphabetic_transformation.java
@@ -0,0 +1,25 @@
+package Study;
+
+import java.util.Scanner;
+
+public class Alphabetic_transformation {
+
+	public static void main(String[] args) {
+		Scanner alphabet = new Scanner(System.in);
+		System.out.print("Enter an integer : ");
+		while (true) {
+			char Alphabet = alphabet.next().charAt(0); // String Alphabet = alphabet.next();
+			if (Alphabet>=65 && Alphabet<=90) {
+				Alphabet += 32;
+				System.out.println(Alphabet);
+			} else if (Alphabet>=97 && Alphabet<=122) {
+				Alphabet -= 32;
+				System.out.println(Alphabet);
+			} else {
+				System.out.println("Invalid input");
+			}
+		}
+
+	}
+
+}
