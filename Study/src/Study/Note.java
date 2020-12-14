985150e96d9bb1c849c43f086099669544d1e7fe
Hyeonmin <kahelum@gmail.com>
2020-12-14 23:20:39 +0900

    commit

--- Study/src/Study/Note.java
+++ Study/src/Study/Note.java
@@ -1,13 +1,21 @@
 package Study;
 
+import java.util.Scanner;
+
 public class Note {
 
 	public static void main(String[] args) {
-		int i;
-		int sum = 0;
-		for (i = 1; i<=100; i++) {
-			sum = sum + i;
+		Scanner in = new Scanner(System.in);
+		int intArray[] = new int[5];
+		int max = 0;
+		
+		for (int i=0; i<5; i++) {
+			intArray[i] = in.nextInt();
+			if (intArray[i] > max) {
+				max = intArray[i];
+			}
 		}
-		System.out.print(sum);
+		System.out.println("Max integer : " + max);
+		
 	}
 }
