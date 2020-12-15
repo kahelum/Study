94f1c6cacaba2de7b0bf36b5b8f96ab2f2bb6fb3
Hyeonmin <kahelum@gmail.com>
2020-12-15 22:59:35 +0900

    commit

--- Study/src/Study/Note.java
+++ Study/src/Study/Note.java
@@ -5,17 +5,8 @@ import java.util.Scanner;
 public class Note {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int intArray[] = new int[5];
-		int max = 0;
-		
-		for (int i=0; i<5; i++) {
-			intArray[i] = in.nextInt();
-			if (intArray[i] > max) {
-				max = intArray[i];
-			}
-		}
-		System.out.println("Max integer : " + max);
+		int Array[][] = new int[2][3];
+		Array[1][2] = 10;
 		
 	}
 }
