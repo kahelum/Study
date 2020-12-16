2fdaf7b64064986e17acf3d5ed9ed69a803c870b
Hyeonmin <kahelum@gmail.com>
2020-12-16 21:48:24 +0900

    commit

--- Study/src/Study/Note.java
+++ Study/src/Study/Note.java
@@ -3,28 +3,7 @@ package Study;
 public class Note {
 
 	public static void main(String[] args) {
-		int Array[][] = new int[4][];
-		Array[0] = new int[4];
-		Array[1] = new int[1];
-		Array[2] = new int[1];
-		Array[3] = new int[4];
-		int sum = 0;
 		
-		for (int i=0; i<Array[0].length; i++) {
-			Array[0][i] = sum++;
-		}
-		Array[1][0] = sum++;
-		Array[2][0] = sum++;
-		for (int i=0; i<Array[3].length; i++) {
-			Array[3][i] = sum++;
-		}
-		
-		for (int i=0; i<Array.length; i++) {
-			for (int j=0; j<Array[i].length; j++) {
-				System.out.print(Array[i][j]);
-			}
-			System.out.println();
-		}
 		
 	}
 }
