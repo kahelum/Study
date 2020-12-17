163e13dc2b5defcee329c03d269a5f58486711d6
Hyeonmin <kahelum@gmail.com>
2020-12-18 03:49:25 +0900

    commit

--- Study/src/Study/Note.java
+++ Study/src/Study/Note.java
@@ -3,18 +3,8 @@ package Study;
 public class Note {
 
 	public static void main(String[] args) {
-		float a[][] = new float[4][];
-		a[0] = new float[4];
-		a[1] = new float[2];
-		a[2] = new float[3];
-		a[3] = new float[4];
 		
-		for (int i=0; i<a.length; i++) {
-			for (int j=0; j<a[i].length; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
+		
 		
 	}
 }
