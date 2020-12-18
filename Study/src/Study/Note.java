c7ef0a12f13674d95941f6ce0b3720cfff90d27d
Hyeonmin <kahelum@gmail.com>
2020-12-18 22:46:40 +0900

    commit

--- Study/src/Study/Note.java
+++ Study/src/Study/Note.java
@@ -1,10 +1,21 @@
 package Study;
 
+import java.util.Scanner;
+
 public class Note {
 
 	public static void main(String[] args) {
+		Scanner star = new Scanner(System.in);
 		
+		System.out.print("Enter an integer : ");
+		int Star = star.nextInt();
 		
+		for(int i=1; i<=Star; i++) {
+			for(int j=Star; j>=i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 		
 	}
 }
