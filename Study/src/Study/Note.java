a4bf0422cd6869a9036889f0a5751d6f1b1cb5de
Hyeonmin <kahelum@gmail.com>
2020-12-28 23:13:08 +0900

    commit

--- Study/src/Study/Note.java
+++ Study/src/Study/Note.java
@@ -1,10 +1,15 @@
 package Study;
 
+import java.util.Scanner;
+
 public class Note {
 
 	public static void main(String[] args) {
 		
-		
-		
+		Scanner in = new Scanner(System.in);
+		float a = in.nextFloat();
+		System.out.printf("%f",a);
+		System.out.println();
+		System.out.print(a);
 	}
 }
