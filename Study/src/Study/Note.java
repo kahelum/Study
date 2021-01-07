dc3440289a4e1d69e15ee15130fae31a6c5cdcf3
Hyeonmin <kahelum@gmail.com>
2021-01-07 23:32:15 +0900

    commit

--- Study/src/Study/Note.java
+++ Study/src/Study/Note.java
@@ -7,11 +7,11 @@ public class Note {
 	public static void main(String[] args) {
 		
 		Scanner in = new Scanner(System.in);
-		String RRN = in.next();
-		String data[] = RRN.split("-");
-		
-		System.out.println(data[0]+data[1]);
-		
-		// a
+		String a = in.next();
+		char arr[] = a.toCharArray();
+		for (char c : arr) {		// foreach ¹®
+			System.out.println("'" + c + "'");
+		}
+		 
 	}
 }
