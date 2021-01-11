6947fb1d4a6f60f6712a40f81ad3665baf5601b0
Hyeonmin <kahelum@gmail.com>
2021-01-11 22:07:46 +0900

    commit

--- Study/src/Study/Note.java
+++ Study/src/Study/Note.java
@@ -7,12 +7,29 @@ public class Note {
 	public static void main(String[] args) {
 		
 		Scanner in = new Scanner(System.in);
-		String a = in.next();
-		char arr[] = a.toCharArray();
-		for (char c : arr) {		// foreach ¹®
-			System.out.println("'" + c + "'");
+		int a[] = new int[5];
+		int input = in.nextInt();
+		int j = 10000;
+		
+		for (int i = 0; i<a.length; i++) {
+			a[i] = (input / j) * j;
+			System.out.println("["+a[i]+"]");
+			input = input % a[i];
+			j = j / 10;
 		}
-		 
-		// for 
 	}
 }
+
+/*
+		Scanner sc = new Scanner(System.in);        
+		String a = sc.next();
+		char [] arr = a.toCharArray();   
+		for(int i=0; i<arr.length; i++) {
+    		System.out.print("[" + arr[i]);
+    		for(int j=arr.length-1; j>i; j--) { 
+        		System.out.print("0");          
+    		}
+    		System.out.println("]");
+		}
+
+*/
