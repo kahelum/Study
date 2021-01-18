671cde71e4fa4c4d79cfa2db6f78bf511ae4e544
Hyeonmin <kahelum@gmail.com>
2021-01-18 21:45:54 +0900

    commit

--- Study/src/Study/Codeup.java
+++ Study/src/Study/Codeup.java
@@ -7,17 +7,9 @@ public class Codeup {
 	public static void main(String[] args) {
 
 		Scanner in = new Scanner(System.in);
-		String time = in.next();
-		String arr[] = time.split("\\.");
-		
-		String year = arr[0];
-		String month = arr[1];
-		String day = arr[2];
-		
-		System.out.println(day+"-"+month+"-"+year);
+		long num = in.nextLong();
+		System.out.println(num);
 		
 	}
 
 }
-
-//
