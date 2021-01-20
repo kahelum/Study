ad46812b1080f98c322b08d099868c9889352f7f
Hyeonmin <kahelum@gmail.com>
2021-01-20 20:35:20 +0900

    commit

--- Study/src/Study/Codeup.java
+++ Study/src/Study/Codeup.java
@@ -7,10 +7,8 @@ public class Codeup {
 	public static void main(String[] args) {
 
 		Scanner in = new Scanner(System.in);
-		long a = in.nextLong();
-		long b = in.nextLong();
-		System.out.println(a+b);
+		char input = in.next().charAt(0);
 		
+		System.out.println((char)(input+1));
 	}
-
 }
