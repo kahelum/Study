834c62d4a5258ace58463830d95b5d411fb9463f
Hyeonmin <kahelum@gmail.com>
2021-01-25 19:52:57 +0900

    commit

--- Study/src/Study/Codeup.java
+++ Study/src/Study/Codeup.java
@@ -9,13 +9,12 @@ public class Codeup {
 		Scanner in = new Scanner(System.in);
 		int a = in.nextInt();
 		int b = in.nextInt();
-		int c = in.nextInt();
 		
-		System.out.println(a+b+c);
-		System.out.printf("%.1f", ((double)a+(double)b+(double)c)/3);
+		a = a << b;
+		
+		System.out.println(a);
 		
 	}
 }
 
 
-//
