8bad1096ce2c7823369367646cfddae8506ef8ee
Hyeonmin <kahelum@gmail.com>
2021-02-07 22:36:33 +0900

    commit

--- Study/src/Study/Codeup.java
+++ Study/src/Study/Codeup.java
@@ -8,19 +8,33 @@ public class Codeup {
 
 		Scanner in = new Scanner(System.in);
 		
-		int score = in.nextInt();
+		int month = in.nextInt();
 		
-		if (score>=90 && score<=100) {
-			System.out.println("A");
-		} else if (score>=70 && score<=89) {
-			System.out.println("B");
-		} else if (score>=40 && score<=69) {
-			System.out.println("C");
-		} else if (score>=0 && score<=39) {
-			System.out.println("D");
+		switch (month) {
+		case 12:
+		case 1:
+		case 2:
+			System.out.println("winter");
+			break;
+		case 3:
+		case 4:
+		case 5:
+			System.out.println("spring");
+			break;
+		case 6:
+		case 7:
+		case 8:
+			System.out.println("summer");
+			break;
+		case 9:
+		case 10:
+		case 11:
+			System.out.println("fall");
+			break;
+		default:
+			break;
 		}
 		
 	}
 }
-//
 
