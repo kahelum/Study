f069f7a681ba703f69e61c7150dda3c664a67d7b
Hyeonmin <kahelum@gmail.com>
2021-02-08 22:33:56 +0900

    commit

--- Study/src/Study/Codeup.java
+++ Study/src/Study/Codeup.java
@@ -5,34 +5,16 @@ import java.util.Scanner;
 public class Codeup {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-		Scanner in = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] num = new int[size];
 		
-		int month = in.nextInt();
-		
-		switch (month) {
-		case 12:
-		case 1:
-		case 2:
-			System.out.println("winter");
-			break;
-		case 3:
-		case 4:
-		case 5:
-			System.out.println("spring");
-			break;
-		case 6:
-		case 7:
-		case 8:
-			System.out.println("summer");
-			break;
-		case 9:
-		case 10:
-		case 11:
-			System.out.println("fall");
-			break;
-		default:
-			break;
+		for (int i = 0; i < num.length; i++) {
+			num[i] = sc.nextInt();
+		}
+		for (int i = 0; i < num.length; i++) {
+			System.out.println(num[i]);
 		}
 		
 	}
